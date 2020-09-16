1898f8c065f06b121d806915cafee1de3503f4e2
Žiga Hajšen <ziga.hajsen@gmail.com>
2020-09-16 19:06:43 +0200

    Added styling while dragging

--- src/column.jsx
+++ src/column.jsx
@@ -15,6 +15,8 @@ const Title = styled.h3`
 `;
 const TaskList = styled.div`
   padding: 8px;
+  background-color: ${(props) => (props.isDraggingOver ? 'skyblue' : 'white')};
+  transition: background-color 0.2s ease;
 `;
 
 export default class Column extends React.Component {
@@ -23,8 +25,12 @@ export default class Column extends React.Component {
       <Container>
         <Title>{this.props.column.title}</Title>
         <Droppable droppableId={this.props.column.id}>
-          {(provided) => (
-            <TaskList ref={provided.innerRef} {...provided.droppableProps}>
+          {(provided, snapshot) => (
+            <TaskList
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              isDraggingOver={snapshot.isDraggingOver}
+            >
               {this.props.tasks.map((task, index) => (
                 <Task key={task.id} task={task} index={index} />
               ))}
